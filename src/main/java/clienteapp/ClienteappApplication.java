e3ccedf6c1bfd9fdc3eb18e1c7579f8d906eaab4
Harby García <harby-96@hotmail.com>
2022-05-07 09:12:50 -0500

    Se ejecuta el proyecto con la template HTML

--- src/main/java/clienteapp/ClienteappApplication.java
+++ src/main/java/clienteapp/ClienteappApplication.java
@@ -2,8 +2,10 @@ package clienteapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"clienteappController"})
 public class ClienteappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/clienteappController/clienteappController.java
+++ src/main/java/clienteappController/clienteappController.java
@@ -3,11 +3,8 @@ package clienteappController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/index")
+@Controller
 public class clienteappController {
 
     @GetMapping("/home")
